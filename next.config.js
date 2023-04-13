927683c300dc9f2118df94ef30571de07fb6bd4b
Łukasz Męderak <lukasz.mederak@gmail.com>
2023-04-13 19:04:45 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  pageExtensions: ["page.tsx", "page.ts"],
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
